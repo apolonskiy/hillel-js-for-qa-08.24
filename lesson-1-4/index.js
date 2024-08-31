cb03dedf1e825dfb137b19bf692900c6ab060e71
Andrii Polonskyi <apolonskyi@alteryx.com>
2024-08-31 17:44:57 +0200

    cleanup with points for next class

--- lesson-1-4/index.js
+++ lesson-1-4/index.js
@@ -1,131 +1,18 @@
-// // Declaring
-// let message;
-// const message1 ='test';
+// NPM INIT, LOCK FILE, GITIGNORE, STRUCTURE OF DEPENDENCIES IN NODE_MODULES
 
-// console.log(message);
+// NPM SCRIPTS
 
-// var abc = 1;
-// var abc = 2;
+// IMPORT / EXPORT (CommonJs and ESM)
 
-// const test = 1;
-// test = 2;
+// ЗМІННІ, ЇХ ВИЗНАЧЕННЯ І ІНІЦІАЛІЗАЦІЯ, ОБЛАСТІ ВИДИМОСТІ
 
-// console.log(var1)
+// Math та інші глобальні об'єкти / Класи (конструктор функції)
 
-// let var1 = 'test1'
-// var var1 = 'test';
+// Строки, різні кавички, екранування, шаблонні строки
 
-// Accessing variable;
-// let message3;
-// message3 = 'Hey';
-// console.log(message3);
+// Порівняння
 
-// let message;
-// message = "Hi";
-
-// let message2 = message;
-// console.log(message2);
-
-// message = 'new value';
-// message2
-
-// const DEFAULT_SITE = 'google.com';
-
-// const test_te$st = 1
-
-
-// _____________________
-
-// let numb = 123 // int
-// console.log( typeof numb)
-// numb = 12.34 // float
-// console.log( typeof numb)
-// console.log(1 / 0);
-
-// console.log('not a number' / 2 + 2);
-// console.log(NaN === NaN)
-
-
-// Strings
-// let oneQuoteStr = 'One quote S';
-// let doubleQuotesStr = "Double quote's String";
-
-// Allows include some statement inside your string call like a tamplate string
-// const qa = "QA";
-// const name = 'Karl';
-// const phrase = `Hello, ${name} the ${qa}, nice to meet you.`;
-// const oldPhrase = 'Hello, '+ name +' nice to meet you';
-// const phraseParagraph = `can add ${name + qa}, 
-// and add Paragraph`;
-
-// console.log(phrase);
-// console.log(oldPhrase);
-// console.log(phraseParagraph);
-
-// Bolleans
-// let truth = true;
-// let lie = false;
-// // && - AND
-// // || - OR
-// // false - 0
-// // true - 1
-// //          0      *    1
-// console.log(false && true)
-// console.log(true && true)
-// console.log(false && false)
-// //            0   +     1
-// console.log(false || true)
-// console.log(false || false)
-
-// // logical NO (!)
-// console.log(!(false && true))
-
-// console.log(9%8.6)
-
-// // Null (empty box)
-// let nothing = null;
-// console.log(nothing)
-
-// // Undefined means the variable has been declared, but it has not yet been assigned a value
-// let emptiness;
-// console.log(emptiness) // Undifined
-
-// const truthy = true
-// console.log(typeof undefined); // undefined
-
-// console.log(typeof 0.5)
-
-// console.log(typeof(truthy))
-
-// console.log(`str ${truthy}`)
-
-// console.log(typeof `str ${truthy}`)
-
-// console.log(typeof Math)
-// console.log(typeof Number)
-// console.log(typeof String)
-// console.log(typeof Object)
-// console.log(typeof Function)
-// console.log(typeof Array)
-// console.log(typeof Map)
-
-// console.log('test'.slice(2,3))
-
-// console.log(typeof Math.pow(3, 3))
-
-// console.log(typeof NaN)
-
-// console.log(typeof [])
-
-// console.log(typeof {})
-
-// console.log(typeof null)
-
-// console.log(typeof console.log)
-
-// console.log(Boolean(-3))
-
-// console.log('sss'.concat('qqq'))
+// Перетворення (явні і неявні)
 
 // class AAA {
 //     alex = {
@@ -152,3 +39,12 @@
 
 // const strEx = new String('testStrEx')
 // console.log(strEx.toString())
+
+// console.log(0 ?? 'test')
+
+// console.log(Boolean('ss'))
+// // ==
+// console.log(!!'ss')
+
+// const ternary = 1 >2 ||true && false ? 'truthy result' : 'falsy result'
+// console.log(ternary)
