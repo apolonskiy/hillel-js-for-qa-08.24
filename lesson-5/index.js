54fa0ec1c7bfae29bcea7fc66ffdfb4c5493a57e
Andrii Polonskyi <apolonskyi@alteryx.com>
2024-09-03 18:53:34 +0200

    add lesson 5 init

---
+++ lesson-5/index.js
@@ -0,0 +1,13 @@
+const test = 'test';
+var test2 = 'test2'
+{
+    console.log(test);
+    // const test = 'new value'
+    var test2 = 'test3'
+    console.log(test);
+    console.log(test2);
+
+}
+console.log(test2);
+console.log(test);
+
