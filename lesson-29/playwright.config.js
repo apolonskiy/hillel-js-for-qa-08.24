122a8223d74907d5b2645afcb255c7da2f9cb7d7
Andrii Polonskyi <apolonskyi@alteryx.com>
2024-12-23 19:22:36 +0100

    configure playwright jobs

--- lesson-29/playwright.config.js
+++ lesson-29/playwright.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
+    ['html', { open: 'never' }  ],
     [
       '@testomatio/reporter/lib/adapter/playwright.js',
       {
