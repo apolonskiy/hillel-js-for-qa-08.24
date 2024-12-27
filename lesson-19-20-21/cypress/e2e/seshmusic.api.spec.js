c188b807def3b552679b9cd650b1c50eb14300ab
Andrii Polonskyi <andriy.polonskiy@gmail.com>
2024-12-27 19:29:29 +0100

    e2e tests setup in actions (#4)
    * e2e tests setup in actions
    
    * fix?
    
    * fix?
    
    * fix tests
    
    * more fixes
    
    * final fix
    
    * more fixes
    
    * more fixes
    
    * more fixes
    
    * try fix html report
    
    * try fix html report
    
    * fixes
    
    ---------
    
    Co-authored-by: Andrii Polonskyi <apolonskyi@alteryx.com>

--- lesson-19-20-21/cypress/e2e/seshmusic.api.spec.js
+++ lesson-19-20-21/cypress/e2e/seshmusic.api.spec.js
@@ -132,7 +132,6 @@ describe('Register with Intercepting with stubbing', () => {
       cy.intercept('POST', '/v1/accounts:signUp*',  (req) => {
         // req.reply({statusCode: 400, body: { idToken: 'invalid' }})
         req.on('response', (res) => {
-          console.log('res.body.email',res.body.email)
           res.body.email = 'fakeEmail@email.com'
           res.send(res.body);
         })
@@ -143,7 +142,6 @@ describe('Register with Intercepting with stubbing', () => {
 
     it('Intercepring registration calls', function() {
       cy.wait('@signUpRequest').then((xhr) => {
-        console.log(xhr);
         //request
         expect(xhr.request.body.email).to.equal(randomUserData.email)
         expect(xhr.request.body.password).to.equal(randomUserData.password)
